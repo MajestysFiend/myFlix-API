6df8107f24213a09fc274bbbdcf1f9896267a7c2
Tristan Lewis <94727522+MajestysFiend@users.noreply.github.com>
2023-04-04 23:11:24 -0400

    added password validation to schema

--- passport.js
+++ passport.js
@@ -17,12 +17,18 @@ passport.use(new LocalStrategy({
             console.log("incorrect username");
             return callback(null, false, { message: "Incorrect username or password" });
         }
+        if (!user.validatePassword(password)) {
+            console.log('incorrect password');
+            return callback(null, false, { message: 'Incorrect password.' });
+        }
+
         console.log('finished');
         return callback(null, user);
     }).catch((error) => {
         return callback(error)
     });
 }
+
 ));
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
